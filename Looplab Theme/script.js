d87b8e3a7170fc3c4463287437264e5ba9c1183a
Alex <axchen93@gmail.com>
2018-09-21 00:42:55 -0400

    inital commit

---
+++ Looplab Theme/script.js
@@ -0,0 +1,18 @@
+//get the current year for copyright
+$('#year').text(new Date().getFullYear());
+        
+//Smooth-Scrolling
+$('#main-nav a').on('click', function(event){
+    if(this.hash !== ""){
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            
+            window.location.hash = hash;
+        });
+    }
+});
